e260682d815839e4d56281dd7e273f30f0bf3bfa
Mihail Paley <undeadlol1@mail.ru>
2016-01-26 08:48:06 +0500

    Adds one readme.md for athmosphere and one for github

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
     git: 'https://github.com/Undeadlol1/angular-filter-count-to',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
-    documentation: 'README.md'
+    documentation: 'METEOR-README.md'
 });
 
 Package.onUse(function(api) {
